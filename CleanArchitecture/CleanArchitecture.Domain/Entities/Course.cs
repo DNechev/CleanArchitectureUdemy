6177f806446ed3c346dc8ee7b219360d8860207b
DNechev <dido94m@gmail.com>
2021-05-30 17:24:47 +0200

    Course Domain Model added

---
+++ CleanArchitecture/CleanArchitecture.Domain/Entities/Course.cs
@@ -0,0 +1,13 @@
+﻿namespace CleanArchitecture.Domain.Entities
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
