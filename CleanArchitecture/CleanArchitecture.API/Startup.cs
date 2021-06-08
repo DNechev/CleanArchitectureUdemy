e742e7544d8e4b667ec8d476a28d31140076ef32
DNechev <dido94m@gmail.com>
2021-06-08 13:33:12 +0200

    Configuring API

--- CleanArchitecture/CleanArchitecture.API/Startup.cs
+++ CleanArchitecture/CleanArchitecture.API/Startup.cs
@@ -1,16 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CleanArchitecture.Infrastructure.IoC;
+using CleanArchitecture.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.API
 {
@@ -32,6 +28,8 @@ namespace CleanArchitecture.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CleanArchitecture.API", Version = "v1" });
             });
+            services.AddDbContext<CleanArchDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ClearnArchDbConnection")));
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +52,11 @@ namespace CleanArchitecture.API
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
